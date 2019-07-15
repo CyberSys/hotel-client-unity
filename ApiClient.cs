06a81210d8e5375541baee6a9a31f199fc47c8a1
josh <joshbothun@gmail.com>
2019-07-15 00:31:15 -0700

    init

---
+++ ApiClient.cs
@@ -0,0 +1,55 @@
+﻿using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class ApiClient
+{
+  private JsonHttpClient client;
+  private string baseHost;
+
+  public ApiClient(string baseHost) {
+    this.client = new JsonHttpClient(new System.Uri(baseHost));
+    this.baseHost = baseHost;
+  }
+
+  public async Task Initialize() {
+    var response = await client.Post<Api.IdentifyResponse>("/identify");
+    if (response.token.Length < 1) {
+      Debug.LogError("Failed to initialize Hotel session!");
+    } else {
+      client.SetSessionToken(response.token);
+    }
+  }
+
+  public async Task<Api.Server[]> ListServers(string gameId) {
+    var url = string.Format("/servers?gameId={0}", gameId);
+    var response = await client.Get<Api.ListServersResponse>(url);
+    return response.servers;
+  }
+
+  public async Task<Api.Server> GetServer(string serverId) {
+    var url = string.Format("/servers/{0}", serverId);
+    var response = await client.Get<Api.Server>(url);
+    return response;
+  }
+
+  public async Task<bool> CreateServer(Api.Server server) {
+    var url = "/servers";
+    var response = await client.Post<Api.Server>(url, server);
+    return true;
+  }
+
+  public async Task<bool> UpdateServer(Api.Server server) {
+    var url = string.Format("/servers/{0}", server.id);
+    var response = await client.Put<Api.Server>(url, server);
+    return true;
+  }
+
+  public async Task<bool> PingServer(Api.Server server) {
+    var url = string.Format("/servers/{0}/alive", server.id);
+    var response = await client.Post<Api.Server>(url);
+    return true;
+  }
+}
---
+++ HotelClient.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using UnityEngine;
+
+public class HotelClient : MonoBehaviour
+{
+  public string masterServerUrl;
+  public string gameId;
+
+  private ApiClient client;
+
+  // Start is called before the first frame update
+  async void Start()
+  {
+    client = new ApiClient(masterServerUrl);
+    await client.Initialize();
+
+    var testServer = new Api.Server();
+    testServer.name = "a";
+    testServer.host = "www.google.com";
+    testServer.port = 2000;
+    testServer.maxPlayers = 4;
+    testServer.numPlayers = 2;
+    testServer.gameId = gameId;
+    await client.CreateServer(testServer);
+    var result = await client.ListServers(gameId);
+    Debug.Log(result.Length);
+  }
+}
